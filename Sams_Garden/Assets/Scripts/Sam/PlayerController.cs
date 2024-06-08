e5246d7eb7b0c81ad6a73b3c8d194c22ef9d51a5
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-06-08 14:19:56 +0300

    Sam Moved Okey

--- Sams_Garden/Assets/Scripts/Sam/PlayerController.cs
+++ Sams_Garden/Assets/Scripts/Sam/PlayerController.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public SOPlayer playerData {  get; private set; }
+    public SOPlayer playerData { get; private set; }
 
+    private Rigidbody2D rb;
 
+    Vector2 movement;
+    float _speed;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        _speed = playerData.Speed;
+    }
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
     private void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + movement * _speed * Time.fixedDeltaTime);
     }
 
 }
