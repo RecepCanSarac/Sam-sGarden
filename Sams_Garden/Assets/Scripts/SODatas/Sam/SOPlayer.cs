dec2bf519dda5b817addcc59b2026c57cd820226
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-06-08 14:05:22 +0300

    Project Started

---
+++ Sams_Garden/Assets/Scripts/SODatas/Sam/SOPlayer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOPlayer : ScriptableObject
+{
+    [Header("Sam Attributes")]
+    public float Health;
+    public float Speed;
+}
---
+++ Sams_Garden/Assets/Scripts/Sam/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public SOPlayer playerData {  get; private set; }
+
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+}
