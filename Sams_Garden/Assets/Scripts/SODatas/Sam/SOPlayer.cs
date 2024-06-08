35ac065ea2694247d9d774d6f9abcb2bd5b68fd1
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-06-08 14:55:41 +0300

    Sam Movent anim and script okey

--- Sams_Garden/Assets/Scripts/SODatas/Sam/SOPlayer.cs
+++ Sams_Garden/Assets/Scripts/SODatas/Sam/SOPlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Player", menuName = "Player/Player")]
 public class SOPlayer : ScriptableObject
 {
     [Header("Sam Attributes")]
--- Sams_Garden/Assets/Scripts/Sam/PlayerController.cs
+++ Sams_Garden/Assets/Scripts/Sam/PlayerController.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public SOPlayer playerData { get; private set; }
+    public SOPlayer playerData;
 
     private Rigidbody2D rb;
-
+    private Animator _animator;
     Vector2 movement;
     float _speed;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         _speed = playerData.Speed;
+        _animator = GetComponent<Animator>();
     }
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        _animator.SetFloat("Horizontal",movement.x);
+        _animator.SetFloat("Vertical",movement.y);
+        _animator.SetFloat("speed", movement.sqrMagnitude);
+
+
     }
     private void FixedUpdate()
     {
